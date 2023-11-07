73e76d872b8925a765e6681c8fc63a22e5769517
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-11-07 14:24:24 +0100

    footer is responsive and has a logo

--- src/components/baselayout/Footer.jsx
+++ src/components/baselayout/Footer.jsx
@@ -1,17 +1,24 @@
 import { Link } from 'react-router-dom';
 import classes from '../../styles/Footer.module.css';
-import PropTypes from 'prop-types';
 import navLinks from '../../data/navLinks.json';
 import { useSelector } from 'react-redux';
 import socials from '../../data/socials.json';
 
-function Header({ }) {
+function Footer({ }) {
 
     const preferredLanguage = useSelector((state) => state.languageReducer);
     return (
         <footer className={classes.footer}>
             <div className={classes.footer_main_content}>
-                <div className={classes.footer_logo}></div>
+                <div className={classes.footer_logo_wrapper}>
+                    <Link to={'/'} title='Home'>
+                        <svg className={classes.footer_logo} viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg" alt="Logo">
+                            <polygon points="0,500 70,500 70,125 190,260 230,210 50,0 0,0" />
+                            <polygon points="105,0 160,65 340,65 395,0" />
+                            <polygon points="500,0 450,0 105,440 105,500 500,500 500,420 210,420 430,130 430,380 500,380" />
+                        </svg>
+                    </Link>
+                </div>
                 <div className={classes.footer_right}>
                     <nav className={classes.footer_right_nav}>
                         {navLinks.map((link) => {
@@ -49,8 +56,4 @@ function Header({ }) {
     )
 }
 
-export default Header;
-
-Header.propTypes = {
-
-}
+export default Footer;
